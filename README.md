f270e47ff91787d87a6f565b61e2898ee63f792b
DiabloAzul <curr33@gmail.com>
2019-09-27 22:26:22 -0600

    readme with gif now

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # bamazon
 mysql/node hw
 
+[GitHub](git@github.com:DiabloAzul33/bamazon.git)
+[Portfolio](https://diabloazul33.github.io/portfolioUpdate/)
+
 
 For this app an online store, Bamazon, is set up with for patrons to visit and make purchases. The intialization of the app immediately asks the user if they would like to make a purchase while also displaying the store's entire inventory so they can see the id #, the price, and how many available of any of the products they desire. 
 
-
 First they must choose the item's id number of whichever product they would like to buy and then they must decide on how many they would like of said item. If they request an available quantity they will be thanked for their purchase as well as notified of overview of what they bought including the cost and quantity. If they request more of a product than is available they will be alerted with a message that there isn't a sufficient quanity and allow them to restart the process.
+
+In this application you will need to install inquirer and mySQL once you cd into the bamazonCustomer.js folder. Using mySQL to create a database and node through Bash/Terminal to interact with the database allowing for purchases and the database to update and calculate the cost of one's purchase.
+
+![bamazon gif](./bamazon2.gif)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,42 +2,71 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-   user: "root",
-   password: "root",
-   host: "localhost",
-   port: 3306,
-   database: "bamazonDB"
+    user: "root",
+    password: "root",
+    host: "localhost",
+    port: 3306,
+    database: "bamazonDB"
 });
 
-function viewAll(){
-    connection.query("SELECT * FROM products", function(err,data){
+function viewAll() {
+    connection.query("SELECT * FROM products", function (err, data) {
         if (err) throw err;
         console.table(data);
+        askUser(data);
     })
- }
+}
 
- viewAll();
- askUser();
+viewAll();
 
- function askUser(){
+function askUser(productDatum) {
 
-     inquirer.prompt([
-         {
-             message: "What item id would you like to purchase?",
-             name: "id"
-            },
-            {
-                message: "How many of the item would you like to purchase?",
-                name: "quantity"
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What item id would you like to purchase?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "How many of the item would you like to purchase?",
+            name: "quantity",
+        }
+    ]).then(function (answers) {
+        var chosenProduct;
+        for (var i = 0; i < productDatum.length; i++) {
+            if (productDatum[i].item_id == answers.id) {
+                chosenProduct = productDatum[i]; 
+                // console.log(chosenProduct);
             }
-        ]).then(function(answers){
-            console.log(answers);
-            connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",  
-                [ answers.quantity, answers.id ],
-             function(err){
-                if (err) throw err;
+        }
+        // console.log(productDatum);
+        if (answers.quantity <= chosenProduct.stock_quantity) {
+
+        connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+            [answers.quantity, answers.id],
+
+        function(err, data) {
+            if (err) throw err;
+
+            if (data.length === 0) {
+				console.log('ERROR: Please select a valid Item ID.');
+                viewAll();
+                askUser();
+            return "Insufficent quantity! Please choose an available amount";
+
+            } else {
+                // var productsData = data[0];
                 
-                console.log(`Thank you for purchasing!`);
-            })
-        });
+              
+                    console.log("Your order for " + chosenProduct.product_name + " costing $ " + parseFloat(chosenProduct.price * answers.quantity).toPrecision(5) + " has been placed. Thank you for your business!");
+                    console.log("\n=================================================\n");
+    
+                    connection.end();
+            }
+        })
+    } else {
+        console.log("Insufficent quantity! Please choose an available amount");
+    }
+});
     }
